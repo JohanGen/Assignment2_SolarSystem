3112b60015704dc4a42f641381f262720733874e
JohanGen <johan.gencher@gmail.com>
2026-03-05 17:59:16 +0100

    yes

--- Assignment2_SolarSystem/Program.cs
+++ Assignment2_SolarSystem/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using Spacesim;
+using SpaceSim;
 
 class Astronomy
 {
@@ -8,11 +8,15 @@ class Astronomy
     {
         List<SpaceObject> solarSystem = new List<SpaceObject>
         {
-            new Star("Sun"),
-            new Planet("Mercury"),
-            new Planet("Venus"),
-            new Planet("Terra"),
-            new Moon("The Moon")
+            new Star("Sun", 0, 0, 696340, 27, "yellow"),
+            new Planet("Mercury", 58, 88, 2440, 59, "gray"),
+            new Planet("Venus", 108, 225, 6052, 243, "yellowish-white"),
+            new Planet("Terra", 150, 365, 6371, 1, "blue"),
+            new Planet("Earth", 150, 365, 6371, 1, "blue"),
+            new Moon("Earth", "The Moon", 384, 27, 1737, 27, "gray"),
+            new Comet("Halley's Comet", 2667, 27375, 11, 52, "dirty white"),
+            new Asteroid("Ceres", 414, 1682, 473, 9, "gray"),
+            new Asteroid_belt("Kuiper belt", 4500, 0, 0, 0, "dark")
         };
 
         foreach (SpaceObject obj in solarSystem)
@@ -24,15 +28,25 @@ class Astronomy
     }
 }
 
-namespace Spacesim
+namespace SpaceSim
 {
     public class SpaceObject
     {
         public String Name { get; protected set; }
+        public int Orbital_radius { get; protected set; }
+        public int Orbital_period { get; protected set; }
+        public int Object_radius { get; protected set; }
+        public int Rotational_period { get; protected set; }
+        public String Object_color { get; protected set; }
 
-        public SpaceObject(String name)
+        public SpaceObject(String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color)
         {
             Name = name;
+            Orbital_radius = orbital_radius;
+            Orbital_period = orbital_period;
+            Object_radius = object_radius;
+            Rotational_period = rotational_period;
+            Object_color = object_color;
         }
         public virtual void Draw()
         {
@@ -41,30 +55,78 @@ namespace Spacesim
     }
     public class Star : SpaceObject
     {
-        public Star(String name) : base(name) { }
+        public Star(String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color) 
+            : base(name, orbital_radius, orbital_period, object_radius, rotational_period, object_color)
+        {
+        }
         public override void Draw()
         {
-            Console.Write("Star  : ");
-            base.Draw();
+            Console.Write("Star         :  " + Name + "  Orbital radius: " + Orbital_radius + "/" + "Orbital period: " + Orbital_period + "/" + "Object radius: " + Object_radius + "km" + "/" + "Rotational period: " + Rotational_period + "/" +  "Object color: " + Object_color);
+            Console.WriteLine();
         }
     }
     public class Planet : SpaceObject
     {
-        public Planet(String name) : base(name) { }
+        public Planet(String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color) 
+            : base(name, orbital_radius, orbital_period, object_radius, rotational_period, object_color)
+        {
+        }
         public override void Draw()
         {
-            Console.Write("Planet:  ");
-            base.Draw();
+            Console.Write("Planet       :  " + Name + "  Orbital radius: " + Orbital_radius + " / " + "Orbital period: " + Orbital_period + " / " + "Object radius: " + Object_radius + "km / " + "Rotational period: " + Rotational_period + " / " + "Object color: " + Object_color);
+            Console.WriteLine();
         }
     }
     public class Moon : Planet
     {
-        public Moon(String name) : base(name) { }
+        public String PlanetName { get; protected set; }
+
+        public Moon(String planetName, String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color) 
+            : base(name, orbital_radius, orbital_period, object_radius, rotational_period, object_color)
+        {
+            PlanetName = planetName;
+        }
 
         public override void Draw()
         {
-            Console.Write("Moon  : ");
-            base.Draw();
+            Console.Write("Moon         :  Planet(" + PlanetName + ")  " + Name + "  Orbital radius: " + Orbital_radius + " / " + "Orbital period: " + Orbital_period + " / " + "Object radius: " + Object_radius + "km / " + "Rotational period: " + Rotational_period + " / " + "Object color: " + Object_color);
+            Console.WriteLine();
+        }
+    }
+    public class Comet : SpaceObject
+    {
+        public Comet(String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color) 
+            : base(name, orbital_radius, orbital_period, object_radius, rotational_period, object_color)
+        {
+        }
+        public override void Draw()
+        {
+            Console.Write("Comet        :  " + Name + "  Orbital radius: " + Orbital_radius + " / " + "Orbital period: " + Orbital_period + " / " + "Object radius: " + Object_radius + "km / " + "Rotational period: " + Rotational_period + " / " + "Object color: " + Object_color);
+            Console.WriteLine();
+        }
+    }
+    public class Asteroid_belt : SpaceObject
+    {
+        public Asteroid_belt(String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color) 
+            : base(name, orbital_radius, orbital_period, object_radius, rotational_period, object_color)
+        {
+        }
+        public override void Draw()
+        {
+            Console.Write("Asteroid Belt:  " + Name + "  Orbital radius: " + Orbital_radius + " / " + "Orbital period: " + Orbital_period + " / " + "Object radius: " + Object_radius + "km / " + "Rotational period: " + Rotational_period + " / " + "Object color: " + Object_color);
+            Console.WriteLine();
+        }
+    }
+    public class Asteroid : Asteroid_belt
+    {
+        public Asteroid(String name, int orbital_radius, int orbital_period, int object_radius, int rotational_period, String object_color) 
+            : base(name, orbital_radius, orbital_period, object_radius, rotational_period, object_color)
+        {
+        }
+        public override void Draw()
+        {
+            Console.Write("Asteroid     :  " + Name + "  Orbital radius: " + Orbital_radius + " / " + "Orbital period: " + Orbital_period + " / " + "Object radius: " + Object_radius + "km / " + "Rotational period: " + Rotational_period + " / " + "Object color: " + Object_color);
+            Console.WriteLine();
         }
     }
 }
